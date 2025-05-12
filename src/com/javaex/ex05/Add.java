81945a7e50ea2187b1fd9e3a8ab1af1b27990167
jkm8369 <jkm8369@naver.com>
2025-05-12 17:52:49 +0900

    2025-05-12 17:52

---
+++ src/com/javaex/ex05/Add.java
@@ -0,0 +1,24 @@
+package com.javaex.ex05;
+
+public class Add {
+    
+	private int a;
+	private int b;
+	
+    public Add(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+
+    
+    public void setValue(int a, int b) {
+    	this.a = a;
+    	this.b = b;
+    }
+
+    public int calculate(int a, int b) {
+    	return a + b;
+    }
+
+}
